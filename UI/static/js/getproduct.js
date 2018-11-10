4d02ac0b9f1a80c6da779ee0460fc347ae61b721
Too Collins <collinstoo.ct@gmail.com>
2018-11-10 20:02:23 +0300

    [Feature #161860847] Consume get all products endpoint

---
+++ UI/static/js/getproduct.js
@@ -0,0 +1,43 @@
+// consume get product endpoint 
+
+window.onload = function getProducts(e) {
+    e.preventDefault();
+
+    let token = localStorage.userToken;
+    fetch('https://a-store-manager-app-api-v2.herokuapp.com/api/v2/products',{
+        method:'GET',
+        headers: {
+            'Content-Type':'application/json',
+            'Authorization': token,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Request-Method': 'GET'
+        }
+    })
+    .then((respose) => respose.json())
+    .then((data) => {
+        let products = data.products;
+        let productsTable = document.getElementById('records-table');
+        th = `
+        <tr>
+        <th>Item ID</th>
+        <th>Item Name</th>
+        <th>Item Price</th>
+        <th>Min Quantity</th>
+        <th>Inventory</th>
+        <th>Category</th>
+        </tr>
+        `
+        productsTable.innerHTML = th
+        products.forEach(product => { 
+            productsTable.innerHTML += '<tr>'+
+                '<td>'+product.product_id+'</td>'+
+                '<td>'+product.name+'</td>'+
+                '<td>'+product.price+'</td>'+
+                '<td>'+product.min_quantity+'</td>'+
+                '<td>'+product.inventory+'</td>'+
+                '<td>'+product.category+'</td>'+
+                '</tr>';
+        })
+    })
+    .catch((err) => console.log(err))
+}
