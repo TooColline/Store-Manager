477e5500fca731549e60a9a8e6f2980f46c5aeb0
Too Collins <collinstoo.ct@gmail.com>
2018-11-10 12:21:17 +0300

    [Feature #161833292] Consume login endpoint and filter roles

---
+++ UI/static/js/login.js
@@ -0,0 +1,65 @@
+
+let url = "https://a-store-manager-app-api-v2.herokuapp.com/api/v2/auth/login";
+let login_button = document.getElementById('login-button');
+let message_box = document.getElementById('message-box');
+
+
+
+login_button.addEventListener('click', (event) => {
+    event.preventDefault();
+    // define the DOM variables to be used in the app
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    message_box.innerHTML = "";
+    login_button.value = "Loading...";
+    login_button.disabled = true;
+
+    let data = {
+        "email": email,
+        "password": password
+    };
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+    .then((response)=> {
+        if(response.status < 500){
+            return response.json()
+        }
+        else{
+            message_box.innerHTML = response;
+        }
+    })
+    .then(function(json_response) {
+        console.log(json_response)
+        let message = json_response.message;
+        if(message === "Successfully logged in") {
+            // Save token and user role on the local storage
+            let userRole = json_response.role;
+            localStorage.userToken = json_response.token;
+            localStorage.userRole = json_response.role;
+            if(userRole === 'Admin'){
+                // Redirect to admin dashboard
+                window.location.replace("UI/templates/admindash.html");
+            }
+            if(userRole === 'attendant'){
+                // Redirect to attendant profile page
+                window.location.replace("UI/templates/myprofile.html");
+            }
+        else {
+            message_box.innerHTML = message;
+            login_button.value = "Login";
+            login_button.disabled = false;
+        }
+    }})
+    .catch(function(error) {
+        message_box.innerHTML = "Sorry. Reload page and try again";
+        console.log(error);
+    });
+});
