0c9f89a08614f860400fe0b62c2d8cc485611d80
Too Collins <collinstoo.ct@gmail.com>
2018-11-10 17:05:49 +0300

    [Feature #161833318] Consume add product endpoint

---
+++ UI/static/js/addproduct.js
@@ -0,0 +1,64 @@
+
+let url = "https://a-store-manager-app-api-v2.herokuapp.com/api/v2/products";
+let addProduct_button = document.getElementById('addProduct_button');
+let message_box = document.getElementById('message-box');
+let token = localStorage.userToken;
+
+addProduct_button.addEventListener('click', (event) => {
+    message_box.innerHTML = "";
+    addProduct_button.disabled = false;
+    addProduct_button.value = "Adding...";
+    let itemName = document.getElementById('name').value;
+    let itemPrice = document.getElementById('price').value;
+    let minQuantity = document.getElementById('min_quantity').value;
+    let inventory = document.getElementById('inventory').value;
+    let itemCategory = document.getElementById('category').value;
+    if(itemName && itemPrice && minQuantity && inventory && itemCategory) {
+        event.preventDefault();
+        let data = {
+            "name": itemName,
+            "price": Number(itemPrice),
+            "min_quantity": Number(minQuantity),
+            "inventory": Number(inventory),
+            "category": itemCategory
+    };
+    // Send POST request to admin login page
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': token
+        }
+    })
+    .then((response)=> {
+        if(response.status < 500){
+            return response.json()
+        }
+        else{
+            message_box.innerHTML = response;
+        }
+    })
+    .then(function(json_response) {
+        let message = json_response.message;
+        if(message === "Product has been added successfully") {
+            message_box.style.color = "green";
+            message_box.innerHTML = `${message}`;
+            addProduct_button.value = "Add another product";
+            addProduct_button.disabled = false;
+        }
+        else {
+            message_box.innerHTML = message;
+            addProduct_button.value = "Add Product";
+            addProduct_button.disabled = false;
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+        message_box.innerHTML = err;
+    });
+    } else {
+        message_box.innerHTML = "Bad request. Request missing a required parameter";
+        addProduct_button.value = "Add Product";
+    }
+}); 
