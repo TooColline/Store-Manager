957bc478a86586ad86d8d4547e86265473d27967
Too Collins <collinstoo.ct@gmail.com>
2018-11-12 20:18:03 +0300

    [Feature #161887796] Consume update product details

--- UI/static/js/getproductedit.js
+++ UI/static/js/getproductedit.js
@@ -36,12 +36,12 @@ window.onload = function getProducts(e) {
         products.forEach(product => { 
             productsTable.innerHTML += '<tr>'+
                 '<td>'+product.product_id+'</td>'+
-                '<td>'+product.name+'</td>'+
-                '<td>'+product.price+'</td>'+
-                '<td>'+product.min_quantity+'</td>'+
-                '<td>'+product.inventory+'</td>'+
-                '<td>'+product.category+'</td>'+
-                '<td><a onclick="getSingleProduct('+product.product_id+')"><div class="btn btn--primary">Edit</div></a></td>'+
+                '<td name="'+product.product_id+'" contenteditable=False>'+product.name+'</td>'+
+                '<td name="'+product.product_id+'" contenteditable=False>'+product.price+'</td>'+
+                '<td name="'+product.product_id+'" contenteditable=False>'+product.min_quantity+'</td>'+
+                '<td name="'+product.product_id+'" contenteditable=False>'+product.inventory+'</td>'+
+                '<td name="'+product.product_id+'" contenteditable=False>'+product.category+'</td>'+
+                '<td><a onclick="getSingleProduct('+product.product_id+')"><div id="'+product.product_id+'" class="btn btn--primary">Edit</div></a></td>'+
                 '<td><a onclick="deleteProduct('+product.product_id+')"><div class="deleteButton">Delete</div></a></td>'+
                 '</tr>';
         })
@@ -56,25 +56,70 @@ let url = "https://a-store-manager-app-api-v2.herokuapp.com/api/v2/products";
 let message_box = document.getElementById('message-box');
 let token = localStorage.userToken;
 
-// function getSingleProduct(product_id) {
-//     let getSingleProductURL = `${url}/${Number(product_id)}`;
+function getSingleProduct(product_id) {
+    let editButton = document.getElementById(product_id).innerHTML;
+    if (editButton == 'Edit'){
+    document.getElementById(product_id).innerHTML = "Save";
+    var elms = document.getElementsByName(product_id);
+    for (var i=0; i < elms.length; i++){
+        elms[i].setAttribute("contenteditable", "True");
+    }
+    }
+    else {
+        document.getElementById(product_id).innerHTML = "Edit";
+        var elms2 = document.getElementsByName(product_id);
+        for (var i=0; i < elms2.length; i++){
+            elms2[i].setAttribute("contenteditable", "false");
+        }
 
-//     fetch(getSingleProductURL, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type':'application/json',
-//             'Authorization': token
-//         }
-//     })
-//     .then((response)=> {
-//         if(response.status < 500){
-//             return response.json()
-//         }
-//         else{
-//             message_box.innerHTML = response;
-//         }
-//     })
-// }
+        let productName = document.getElementsByName(product_id)[0].innerHTML;
+        let productPrice = document.getElementsByName(product_id)[1].innerHTML;
+        let productMinQuantity = document.getElementsByName(product_id)[2].innerHTML;
+        let productInventory = document.getElementsByName(product_id)[3].innerHTML;
+        let productCategory = document.getElementsByName(product_id)[4].innerHTML;
+
+        let data = {
+            "name": productName,
+            "price": Number(productPrice),
+            "min_quantity": Number(productMinQuantity),
+            "inventory": Number(productInventory),
+            "category": productCategory
+        };
+        fetch(`${url}/${Number(product_id)}`, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': token
+            }
+        })
+        .then((response)=> {
+            if(response.status < 500){
+                return response.json()
+            }
+            else{
+                message_box.innerHTML = response;
+            }
+        })
+        .then(function(json_response) {
+            let message = json_response.message;
+            if(message === "Product has been modified successfully") {
+                window.location.replace("modifyproducts.html");
+            }
+            else if (json_response.Message === "You need to login" ||
+                     json_response.Message === "This token is invalid") {
+                window.location.replace("../../index.html");
+            }
+            else {
+                message_box.innerHTML = message;
+            }
+        })
+        .catch(function(err) {
+            console.log(err);
+            message_box.innerHTML = err;
+        });
+    }
+}
 
 function deleteProduct(product_id) {
     let deleteurl = `${url}/${Number(product_id)}`;
